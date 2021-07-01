e0c67e91cd83693362110c6e4054aff0886cb288
KristijanWeb <44787915+KristijanWeb@users.noreply.github.com>
2021-07-01 11:26:14 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -13,20 +13,3 @@ conn.connect(function(err) {
 });
 
 module.exports = conn;
-
-
-// const mysql = require('mysql');
-
-// const conn = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'kristij2_nodejsuser',
-//   password: 'Nodejsuser;;', 
-//   database: 'kristij2_nodejs' 
-// });
-
-// conn.connect(function(err) {
-//   if (err) throw err;
-//   console.log('Database is connected successfully !');
-// });
-
-// module.exports = conn;
